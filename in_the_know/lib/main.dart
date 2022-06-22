270b20f19378afa9036aa68ca960394f01da13dc
Marko Mamić <marko.mamic0608@gmail.com>
2022-06-22 20:19:15 +0200

    It works (item title and image)

--- in_the_know/lib/main.dart
+++ in_the_know/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:in_the_know/rss_utils.dart' as utils;
+import 'package:webfeed/webfeed.dart';
 
 void main() {
-  dynamic x = utils.returnFeed("https://www.nytimes.com/");
-  if (kDebugMode) {
-    print(x.description);
-  }
   runApp(const MyApp());
 }
 
@@ -54,18 +51,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String _url = "https://www.nytimes.com/";
 
   @override
   Widget build(BuildContext context) {
@@ -82,40 +68,40 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: FutureBuilder<RssFeed>(
+              future: utils.findFeed(_url),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data?.items?.length,
+                    itemBuilder: (BuildContext context, int position) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      snapshot.data!.items![position].title!,
+                                      style: Theme.of(context).textTheme.headlineSmall,
+                                      ),
+                                  ),
+                                  Image.network(snapshot?.data?.items?[position].media?.contents?.length !=0? snapshot.data!.items![position].media!.contents!.first!.url! : snapshot.data!.image!.url! )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+              })),
     );
   }
 }
--- in_the_know/lib/rss_utils.dart
+++ in_the_know/lib/rss_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:feed_finder/feed_finder.dart';
 import 'package:flutter/foundation.dart';
 import 'package:webfeed/webfeed.dart';
@@ -6,15 +8,17 @@ Future<RssFeed> findFeed(var url) async {
   var feedUrl = await FeedFinder.scrape(url);
   final client = http.Client();
   final response = await client.get(Uri.parse(feedUrl.first));
-  var y = RssFeed.parse(response.body);
+  var y = RssFeed.parse(utf8.decode(response.bodyBytes));
   client.close();
   return y;
+
 }
 
-RssFeed returnFeed(var url) {
+/*RssFeed returnFeed(var url) {
   findFeed(url).then((data) {}, onError: (e){if (kDebugMode) {
     print(e);
   }
   });
   return data;
 }
+*/
